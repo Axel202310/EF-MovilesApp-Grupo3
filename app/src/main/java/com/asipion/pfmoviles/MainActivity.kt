6440ecdf0a35c718bfa1543f07da06d5a6414079
Yuriko Matsuo <N00305488@upn.pe>
2025-06-04 06:29:18 -0500

    Cambio Mirella: Interfaz agregartransferencia

--- app/src/main/java/com/asipion/pfmoviles/MainActivity.kt
+++ app/src/main/java/com/asipion/pfmoviles/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.asipion.pfmoviles
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etDate: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        // Inicializar la vista después de setContentView
+        //etDate = findViewById(R.id.etDate)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Mostrar el calendario al hacer clic
+        etDate.setOnClickListener {
+            showDatePickerDialog()
+        }
+    }
+
+    private fun showDatePickerDialog() {
+        val datePicker = DatePickerFragment { day, month, year -> onDateSelected(day, month, year) }
+        datePicker.show(supportFragmentManager, "datePicker")
+    }
+    private fun onDateSelected(day: Int, month: Int, year: Int) {
+        etDate.setText("$day / $month / $year")
     }
 }
