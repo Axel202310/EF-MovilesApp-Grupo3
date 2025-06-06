c451c45c04930f313c93ffbb0b3b5387b830eb27
Yuriko Matsuo <N00305488@upn.pe>
2025-06-06 17:07:12 -0500

    Cambio Mirella:modificar categorias

---
+++ app/src/main/java/com/asipion/pfmoviles/ModificarCategoriaActividad.kt
@@ -0,0 +1,13 @@
+package com.asipion.pfmoviles
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ModificarCategoriaActividad: AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.modificar_categoria)
+    }
+}
