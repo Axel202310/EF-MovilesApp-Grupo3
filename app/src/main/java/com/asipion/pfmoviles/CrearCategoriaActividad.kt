a4a6f81853dd9956c369766a6ef7ced1e494189d
Yuriko Matsuo <N00305488@upn.pe>
2025-06-06 13:32:47 -0500

    Cambio Mirella:  ya esta todo lo de añadir categoria

---
+++ app/src/main/java/com/asipion/pfmoviles/CrearCategoriaActividad.kt
@@ -0,0 +1,11 @@
+package com.asipion.pfmoviles
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CrearCategoriaActividad: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.crear_categoria) //er
+    }
+}
