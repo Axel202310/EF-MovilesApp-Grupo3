cb196e8452b1e75e49025719329b06f17a237902
Yuriko Matsuo <N00305488@upn.pe>
2025-06-06 18:40:10 -0500

    Cambio Mirella:panel inicio

---
+++ app/src/main/java/com/asipion/pfmoviles/MenuPrincipalActividad.kt
@@ -0,0 +1,22 @@
+package com.asipion.pfmoviles
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MenuPrincipalActividad : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu_principal)
+        ircategorias()
+    }
+    private fun ircategorias(){
+        val btnCategoria = findViewById<Button>(R.id.btnCategoria)
+
+        btnCategoria.setOnClickListener {
+            val intent = Intent(this, MenuCategoriasActividad::class.java)
+            startActivity(intent)
+        }
+    }
+}
